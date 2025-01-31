b74e323e5ede4a7f101e06511ac1508833f9d07c
GINA <101798990+FairyGina@users.noreply.github.com>
2025-01-31 21:05:42 +0900

    [Bronze V] Title: 꼬마 정민, Time: 96 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        long a=sc.nextLong(),b=sc.nextLong(),c=sc.nextLong();
+        System.out.print(a+b+c);
+    }
+}
