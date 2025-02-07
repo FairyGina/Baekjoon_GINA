8478b57107745d3d74c9d533ad5c188bf83ccc2f
GINA <101798990+FairyGina@users.noreply.github.com>
2025-02-08 03:05:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 25784 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,40 +1,27 @@
 import java.util.*;
 class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner s=new Scanner(System.in);
-        int n=s.nextInt(),k=s.nextInt(),i=0,r=0,MAX=100001;
+        int n=s.nextInt(),k=s.nextInt(),r=0,M=100001;
+        boolean[] T=new boolean[M];
         Queue<Integer> q=new LinkedList<>();
-        boolean[] T=new boolean[MAX];
-        ArrayList<ArrayList<Integer>> a=new ArrayList<>(n);
-        while(i<MAX){
-            a.add(new ArrayList<>());
-            a.get(i).add(i-1);
-            a.get(i).add(i+1);
-            a.get(i).add(2*i);
-            i++;
-        } // 인접리스트 준비
         q.offer(n);
-        L: do{
-            int z=q.size();
-            i=0;
-            do { 
-                int x=q.poll();
+        L:do{
+            int z=q.size(), i=0;
+            do {
                 i++;
-                if (x==k) break L;
-                else{
-                    ArrayList<Integer> d = a.get(x);
-                    for(int j=0;j<3;j++){
-                        if((d.get(j)<0)||(d.get(j)>=MAX)) continue;
-                        else if(T[d.get(j)]!=false) continue;
-                        else{
-                            T[d.get(j)]=true;
-                            q.offer(d.get(j));
-                        }
-                    }
+                int x=q.poll();
+                if(x==k) break L;
+                else if(((x<0)||(x>=M))||(T[x]==true)) continue;
+                else {
+                    T[x]=true;
+                    q.offer(x*2);
+                    q.offer(x-1);
+                    q.offer(x+1);
                 }
             } while (i<z);
             r++;
-        }while(true);
+        }while(!(q.isEmpty()));
         System.out.print(r);
     }
 }
