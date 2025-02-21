7de556f7abfcdb6cdc65aa017fa481e06858e52c
GINA <101798990+FairyGina@users.noreply.github.com>
2025-02-21 21:54:02 +0900

    [Bronze II] Title: 패리티, Time: 60 ms, Memory: 11400 KB -BaekjoonHub

--- 백준/Bronze/4597. 패리티/패리티.java
+++ 백준/Bronze/4597. 패리티/패리티.java
@@ -3,18 +3,15 @@ class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder s=new StringBuilder(I.readLine());
-        while(!"#".equals(s.toString())){
+        StringBuilder s;
+        while(!(s=new StringBuilder(I.readLine())).toString().equals("#")){
             int l=s.length()-1,r=0;
-            for(int i=0;i<l;i++){
-                r+=s.charAt(i)-'0';
-            }
+            for(int i=0;i<l;i++) r+=s.charAt(i)-'0';
             boolean b=false,d=false;
             if(s.charAt(l)=='e') b=true;
             if(r%2==0) d=true;
             s.setCharAt(l,(b^d) ?'1':'0');
             O.write(s.toString()+"\n");
-            s=new StringBuilder(I.readLine());
         }
         O.flush();
     }
