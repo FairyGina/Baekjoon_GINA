ec8eb4dd67230b6ecdadf502e6adaf8f75197b4c
GINA <101798990+FairyGina@users.noreply.github.com>
2025-03-16 23:13:40 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(I.readLine());
+        O.write(n*10/11+"\n");
+        O.flush();
+    }
+}
