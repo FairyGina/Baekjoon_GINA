900d2538ecf6d90e0f3512189ea2c0ac462328a5
GINA <101798990+FairyGina@users.noreply.github.com>
2025-03-24 16:13:04 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 132 ms, Memory: 13036 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(I.readLine());
+        while(n-->0) O.write('a');
+        O.write("\n");
+        O.flush();      
+    }
+}
