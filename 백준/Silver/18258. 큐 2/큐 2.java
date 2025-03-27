479d2d56ff0beeba23fbcf46d88e6353f8283448
GINA <101798990+FairyGina@users.noreply.github.com>
2025-03-27 22:44:15 +0900

    [Silver IV] Title: 큐 2, Time: 1352 ms, Memory: 357048 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -3,69 +3,30 @@ import java.util.*;
 import java.util.function.*;
 
 class Main{
-    static ArrayList<Integer>q,a;
-    static int f=-1,b=-1;
-
-    public static void pr(int x){
-        if(b==f) a.add(-1);
-        else a.add(q.get(x));
+    static Deque<Integer> d;
+    public static int e(Supplier<Integer> s){
+        return d.isEmpty()?-1:s.get();
     }
-
-    public static void empty(){
-        a.add((b==f)?1:0);
-    }
-
-    public static void push(int x){
-        b++;
-        q.add(x);
-    }
-
-    public static void pop(){
-        front();
-        if(f!=b) f++;
-    }
-
-    public static void size(){
-        a.add(b-f);
-    }
-
-    public static void front(){
-        pr(f+1);
-    }
-
-    public static void back(){
-        pr(b);
-    }
-
     public static void main(String[] args)throws Exception{
-        
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
-
         Map<String,Consumer<Integer>>c=new HashMap<>();
-        Map<String,Runnable> m=new HashMap<>();
-
-        c.put("push",Main::push);
-        m.put("pop",Main::pop);
-        m.put("size",Main::size);
-        m.put("empty",Main::empty);
-        m.put("front",Main::front);
-        m.put("back",Main::back);
-
+        Map<String,Supplier<Integer>>m=new HashMap<>();
+        c.put("push",x->d.addLast(x));
+        m.put("pop",()->e(d::pollFirst));
+        m.put("size",()->d.size());
+        m.put("empty",()->d.isEmpty()?1:0);
+        m.put("front",()->e(d::peekFirst));
+        m.put("back",()->e(d::peekLast));
         int n=Integer.parseInt(I.readLine());
-        q=new ArrayList<>(n); a=new ArrayList<>(n);
-
-
-        for(int i=0;i<n;i++){
+        d=new ArrayDeque<>(n);
+        while(n-->0){
             String s=I.readLine();
             if(s.contains(" ")){
                 String[] t=s.split(" ");
                 c.get(t[0]).accept(Integer.valueOf(t[1]));
-            }
-            else m.get(s).run();
+            } else O.write(m.get(s).get()+"\n");
         }
-        n=a.size();
-        for(int i=0;i<n;i++) O.write(a.get(i)+"\n");
         O.flush();
     }
 }
