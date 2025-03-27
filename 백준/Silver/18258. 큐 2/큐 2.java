2ea6d082ddeae1dcbb2cd301d24f967b99770403
GINA <101798990+FairyGina@users.noreply.github.com>
2025-03-27 23:25:42 +0900

    [Silver IV] Title: 큐 2, Time: 1312 ms, Memory: 366232 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,36 +1,32 @@
 import java.io.*;
 import java.util.*;
 import java.util.function.*;
-
 class Main{
     static Deque<Integer> d;
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
-
         Map<String,Consumer<Integer>>c=new HashMap<>();
         Map<String,Supplier<Integer>>m=new HashMap<>();
-
         c.put("push",x->d.addLast(x));
         m.put("pop",()->d.isEmpty()?-1:d.pollFirst());
         m.put("size",()->d.size());
         m.put("empty",()->d.isEmpty()?1:0);
         m.put("front",()->d.isEmpty()?-1:d.peekFirst());
         m.put("back",()->d.isEmpty()?-1:d.peekLast());
-
         int n=Integer.parseInt(I.readLine());
         d=new ArrayDeque<>(n);
-
+        StringBuilder a=new StringBuilder();
         while(n-->0){
             String s=I.readLine();
             if(s.contains(" ")){
                 String[] t=s.split(" ");
                 c.get(t[0]).accept(Integer.valueOf(t[1]));
             } else{
-                O.write(m.get(s).get()+"\n");
+                a.append(m.get(s).get()).append("\n");
             }
         }
+        O.write(a+"\n");
         O.flush();
-
     }
 }
