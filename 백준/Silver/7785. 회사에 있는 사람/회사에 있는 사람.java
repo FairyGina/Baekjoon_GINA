b335d8803b1c0cf178fab9982726338feace9183
GINA <101798990+FairyGina@users.noreply.github.com>
2025-03-28 14:59:35 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 540 ms, Memory: 67008 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -5,14 +5,17 @@ class Main{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
         int n=Integer.parseInt(I.readLine());
-        Set<String>t=new TreeSet<>();
+        Set<String>t=new TreeSet<>(Collections.reverseOrder());
         while(n-->0){
             String s=I.readLine();
             String[] g=s.split(" ");
-            if(g[1].equals("enter")) t.add(g[0]);
-            else t.remove(g[0]);
+            if(g[1].equals("enter")){
+                t.add(g[0]);
+            }else{
+                t.remove(g[0]);
+            }
         }
-        Iterator<String> i =((TreeSet<String>)t).descendingIterator();
+        Iterator i =t.iterator();
         StringBuilder a=new StringBuilder();
         while(i.hasNext())a.append(i.next()).append("\n");
         O.write(a+"\n");
