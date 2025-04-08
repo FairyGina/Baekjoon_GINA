4b4ea683643715d8eb5560a1a637fdd1df555212
GINA <101798990+FairyGina@users.noreply.github.com>
2025-04-08 17:46:12 +0900

    [Gold V] Title: 공약수, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Gold/2436. 공약수/공약수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static int gcd(int a,int b){
+        return b==0?a:gcd(b,a%b);
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder a=new StringBuilder();
+        StringTokenizer s=new StringTokenizer(I.readLine());
+        int q=Integer.parseInt(s.nextToken()),w=Integer.parseInt(s.nextToken()),m=100000000,x=0,y=0,ax=0,ay=0,e=w/q;
+        for(int i=1;i*i<=e;i++){
+            x=i*q; y=e/i*q;
+            int t=x+y;
+            if(e%i==0&&t<m&&gcd(x,y)==q){
+                ax=x; ay=y;
+                m=t;
+            }
+        }
+        a.append(ax).append(' ').append(ay);
+        O.write(a+"\n");
+        O.flush();
+    }
+}
