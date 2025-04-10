ab9dc5c302480019178306766e4baf75a2a8e596
GINA <101798990+FairyGina@users.noreply.github.com>
2025-04-10 23:21:03 +0900

    [Bronze V] Title: A+B, Time: 80 ms, Memory: 12296 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,32 @@
+import java.io.*;
+class Main{
+    static StringBuilder a=new StringBuilder();
+    public static void add(String s1,String s2){
+        int la=s1.length(),lb=s2.length(),m=0;
+        if(la>lb){
+            m=la;
+            String t=String.format("%"+m+"s",s2).replace(' ','0');
+            s2=t;
+        } else{
+            m=lb;
+            String t=String.format("%"+m+"s",s1).replace(' ','0');
+            s1=t;
+        }
+        boolean e=false;
+        m--;
+        for(int i=m;i>-1;i--){
+            int t=s1.charAt(i)-'0'+(s2.charAt(i)-'0');
+            a.insert(0,(t+(e==true?1:0))%10);
+            if((t+(e==true?1:0))/10>0) e=true; else e=false;
+        }
+        if(e==true)a.insert(0,1);
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
+        String s1=I.readLine(),s2=I.readLine();
+        add(s1,s2);
+        O.write(a+"\n");
+        O.flush();
+    }
+}
