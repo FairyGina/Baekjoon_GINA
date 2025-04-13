afd06df9769dfcf48d0ea89456968343d0cc1ead
GINA <101798990+FairyGina@users.noreply.github.com>
2025-04-13 23:32:48 +0900

    [Bronze IV] Title: 2033년 밈 투표, Time: 68 ms, Memory: 11696 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Set<String> m=new HashSet<>();
+
+        m.add("Never gonna give you up");
+        m.add("Never gonna let you down");
+        m.add("Never gonna run around and desert you");
+        m.add("Never gonna make you cry");
+        m.add("Never gonna say goodbye");
+        m.add("Never gonna tell a lie and hurt you");
+        m.add("Never gonna stop");
+
+        String as="No";
+
+        int t=Integer.parseInt(I.readLine());
+        while(t-->0){
+            String s=I.readLine();
+            if(!m.contains(s)) as="Yes";
+        }
+        O.write(as+"\n");
+        O.flush();
+    }
+}
