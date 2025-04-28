7cc3e3ceb95a9d8cc48c1df0605a6360f576a1ed
GINA <101798990+FairyGina@users.noreply.github.com>
2025-04-29 01:57:59 +0900

    [Silver II] Title: 예산, Time: 96 ms, Memory: 13784 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,20 +1,16 @@
 import java.io.*;
 import java.util.*;
-
 class Main{
     static int[] lc;
     static int n;
-    public static int fd(int as){
-        int l=0,r=n-1,md=0;
-        while(l<=r){
-            md=(l+r)/2;
-            if(lc[md]<as){
-                l=md+1;
-            }else if(lc[md]>as){
-                r=md-1;
-            }else break;
+    public static int ub(int as){
+        int l=0,r=n;
+        while(l<r){
+            int m=(l+r)/2;
+            if(lc[m]<=as) l=m+1;
+            else r=m;
         }
-        return (l+r)/2;
+        return l-1;
     }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
@@ -25,25 +21,18 @@ class Main{
         Arrays.sort(lc);
         int[] nu=lc.clone();
         for(int i=1;i<n;i++) nu[i]+=nu[i-1];
-        int cs=Integer.parseInt(I.readLine()),mn=0,mx=nu[n-1];
-        int as=0;
+        int cs=Integer.parseInt(I.readLine()),mn=0,mx=lc[n-1],as=0;
         while(mn<=mx){
-            as=(mn+mx)/2;
-            int md=fd(as);
-            int t=nu[md]+as*(n-md-1);
-            if(t<cs){
-                mn=as+1;
-            }else if(t>cs){
-                mx=as-1;
-            }else break;
-        }
-        as=(mn+mx)/2;
-        if(cs/n<lc[0]){
-            as=cs/n;
-        }else if(as>lc[n-1]){
-            as=lc[n-1];
+            int md=(mn+mx)/2;
+            int p=ub(md);
+            int t=(p<0?0:nu[p])+md*(n-p-1);
+            if(t<=cs){
+                as=md;
+                mn=md+1;
+            }else{
+                mx=md-1;
+            }
         }
         System.out.println(as);
-        
     }
 }
