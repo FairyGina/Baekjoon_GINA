d054b56da09c2459b935c6d94c176c6ca1446884
GINA <101798990+FairyGina@users.noreply.github.com>
2025-04-30 01:22:14 +0900

    [Silver IV] Title: 다음 소수, Time: 152 ms, Memory: 11920 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -1,24 +1,25 @@
 import java.io.*;
 class Main{
+    public static boolean ip(long n){
+        if(n<2) return false;
+        if(n<4) return true;
+        if(n%2==0||n%3==0) return false;
+        for(long i=5;i*i<=n;i+=6) if(n%i==0||n%(i+2)==0) return false;
+        return true;
+    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder as=new StringBuilder();
         int t=Integer.parseInt(I.readLine());
         while(t-->0){
             long n=Long.parseLong(I.readLine());
-            if(n<2) n=2;
-            else{
-                L:while(true){
-                    for(long i=2;i*i<=n;i++){
-                        if(n%i==0){
-                            n++;
-                            continue L;
-                        } 
-                    }
+            while(true){
+                if(ip(n)){
+                    as.append(n).append('\n');
                     break;
                 }
+                n++;
             }
-            as.append(n).append('\n');
         }
         System.out.println(as);
     }
