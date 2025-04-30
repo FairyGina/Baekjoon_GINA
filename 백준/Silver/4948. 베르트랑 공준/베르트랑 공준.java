17afe7650611935302fcbf8c784bf079785c51f3
GINA <101798990+FairyGina@users.noreply.github.com>
2025-04-30 23:51:52 +0900

    [Silver II] Title: 베르트랑 공준, Time: 92 ms, Memory: 12708 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,7 +1,6 @@
 import java.io.*;
 class Main{
     public static boolean iP(int n){
-        if(n<4) return true;
         if(n%2==0||n%3==0) return false;
         for(int i=5;i*i<=n;i+=6){
             if(n%i==0||n%(i+2)==0) return false;
@@ -11,13 +10,16 @@ class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder as=new StringBuilder();
+        int sz=246913;
+        int[] nu=new int[sz];
+        nu[1]=1; nu[2]=2; nu[3]=3; nu[4]=3;
+        for(int i=5;i<sz;i++){
+            if(iP(i)) nu[i]=nu[i-1]+1;
+            else nu[i]=nu[i-1];
+        }
         int n=Integer.parseInt(I.readLine());
         while(n!=0){
-            int l=2*n,r=0;
-            for(int i=l;i>n;i--){
-                if(iP(i)) r++;
-            }
-            as.append(r).append('\n');
+            as.append(nu[2*n]-nu[n]).append('\n');
             n=Integer.parseInt(I.readLine());
         }
         System.out.println(as);
