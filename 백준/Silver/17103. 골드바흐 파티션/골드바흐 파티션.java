9edd63eae9d8c207810d715f4b89be06c023d496
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-01 23:29:12 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 144 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -4,20 +4,23 @@ class Main{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder as=new StringBuilder();
         boolean[] b=new boolean[1000001];
-        int t=Integer.parseInt(I.readLine());
+        int t=Integer.parseInt(I.readLine()),pi=0;
+        int[] pn=new int[78498];
         b[0]=b[1]=true;
-        for(int i=2;i*i<=1000000;i++){
+        for(int i=2;i*i<1000001;i++){
             if(!b[i]){
-                for(int j=i*i;j<=1000000;j+=i){
+                for(int j=i*i;j<1000001;j+=i){
                     b[j]=true;
                 }
             }
         }
+        for(int i=0;i<1000001;i++){
+            if(!b[i]) pn[pi++]=i;
+        }
         while(t-->0){
-            int n=Integer.parseInt(I.readLine()),r=0;
-            int l=n/2;
-            for(int i=2;i<=l;i++){
-                if(!(b[i]||b[n-i])) r++;
+            int n=Integer.parseInt(I.readLine()),r=0,l=n/2;
+            for(int i=0;i<pi&&pn[i]<=l;i++){
+                if(!(b[n-pn[i]])) r++;
             }
             as.append(r).append("\n");
         }
