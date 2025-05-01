6690a0804c01c6341fa3e2557506b3e7e4a4cdc2
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-01 20:39:36 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 176 ms, Memory: 13496 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,28 +1,26 @@
 import java.io.*;
 class Main{
-    static boolean[] b=new boolean[1000001];
-    public static boolean iP(int n){
-        if(n%2==0||n%3==0) return false;
-        for(int i=5;i*i<=n;i+=6){
-            if(n%i==0||n%(i+2)==0) return false;
-        }
-        return true;
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder as=new StringBuilder();
+        boolean[] b=new boolean[1000001];
         int t=Integer.parseInt(I.readLine());
-        for(int i=5;i<1000001;i++) b[i]=iP(i);
-        b[2]=true; b[3]=true;
+        b[0]=b[1]=true;
+        for(int i=2;i*i<=1000000;i++){
+            if(!b[i]){
+                for(int j=i*i;j<=1000000;j+=i){
+                    b[j]=true;
+                }
+            }
+        }
         while(t-->0){
             int n=Integer.parseInt(I.readLine()),r=0;
             int l=n/2;
-            if(n<9) r=1;
-            else if(b[3]&&b[n-3]) r++;
-            for(int i=5;i<=l;i+=6) if(b[i]&&b[n-i]) r++;
-            for(int i=7;i<=l;i+=6) if(b[i]&&b[n-i]) r++;
-            O.write(r+"\n");
+            for(int i=2;i<=l;i++){
+                if(!(b[i]||b[n-i])) r++;
+            }
+            as.append(r).append("\n");
         }
-        O.flush();
+        System.out.println(as);
     }
 }
