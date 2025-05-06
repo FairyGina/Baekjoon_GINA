58ec81d9dfdc9e587af3f33077fcab9596674969
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-07 00:42:46 +0900

    [Silver V] Title: 창문 닫기, Time: 64 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(I.readLine()),r=1,i=0;
+        while(r<=n) r+=2*i+++3;
+        System.out.println(i);
+    }
+}
