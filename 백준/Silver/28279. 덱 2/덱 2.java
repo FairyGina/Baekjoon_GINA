970b4c541c33b46b027890cb71b71d5fe0e2e3f7
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-08 14:35:55 +0900

    [Silver IV] Title: 덱 2, Time: 1004 ms, Memory: 239628 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -1,34 +1,10 @@
 import java.io.*;
 import java.util.*;
 class Main{
-    static Deque<Integer> dq=new ArrayDeque<>(1000000);
-    static boolean et(){
-        return dq.isEmpty();
-    }
-    static int sz(){
-        return dq.size();
-    }
-    static void fo(int x){
-        dq.offerFirst(x);
-    }
-    static void eo(int x){
-        dq.offerLast(x);
-    }
-    static int fp(){
-        return et()?-1:dq.poll();
-    }
-    static int ep(){
-        return et()?-1:dq.pollLast();
-    }
-    static int fl(){
-        return et()?-1:dq.peek();
-    }
-    static int el(){
-        return et()?-1:dq.peekLast();
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder as=new StringBuilder();
+        Deque<Integer> dq=new ArrayDeque<>(1000000);
         int n=Integer.parseInt(I.readLine());
         while(n-->0){
             StringTokenizer st=new StringTokenizer(I.readLine());
@@ -36,28 +12,28 @@ class Main{
             if(st.hasMoreTokens()) x=Integer.parseInt(st.nextToken());
             switch(f){
                 case 1:
-                    fo(x);
+                    dq.offerFirst(x);
                     break;
                 case 2:
-                    eo(x);
+                    dq.offerLast(x);
                     break;
                 case 3:
-                    as.append(fp()).append('\n');
+                    as.append(dq.isEmpty()?-1:dq.poll()).append('\n');
                     break;
                 case 4:
-                    as.append(ep()).append('\n');
+                    as.append(dq.isEmpty()?-1:dq.pollLast()).append('\n');
                     break;
                 case 5:
-                    as.append(sz()).append('\n');
+                    as.append(dq.size()).append('\n');
                     break;
                 case 6:
-                    as.append(et()==true?1:0).append('\n');
+                    as.append(dq.isEmpty()?1:0).append('\n');
                     break;
                 case 7:
-                    as.append(fl()).append('\n');
+                    as.append(dq.isEmpty()?-1:dq.peek()).append('\n');
                     break;
                 default:
-                    as.append(el()).append('\n');
+                    as.append(dq.isEmpty()?-1:dq.peekLast()).append('\n');
                     break;
             }
         }
