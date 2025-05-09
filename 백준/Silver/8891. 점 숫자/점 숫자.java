4483c292f5250ea1147f2c3493460e52abb1dfb7
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-09 20:51:21 +0900

    [Silver IV] Title: 점 숫자, Time: 140 ms, Memory: 17268 KB -BaekjoonHub

--- 백준/Silver/8891. 점 숫자/점 숫자.java
+++ 백준/Silver/8891. 점 숫자/점 숫자.java
@@ -1,37 +1,26 @@
 import java.io.*;
 import java.util.*;
 class Main{
-    static int ag=20001,x=0,y=0;
-    static int[] dp=new int[ag];
-    static int bs(int n){
-        int l=1,r=ag-1;
-        while(!(r-l==1)){
-            int m=(l+r)/2;
-            if(dp[m]==n) return m;
-            if(dp[m]>n) r=m;
-            else l=m;
-        }
-        return r;
-    }
-    static void fc(int n){
-        int id=bs(n);
-        int ms=dp[id]-n;
-        x+=id-1-ms;
-        y+=1+ms;
-    }
+    static int ag=10001;
+    static int[] xa=new int[ag],ya=new int[ag];
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder as=new StringBuilder();
-        for(int i=2;i<ag;i++){ //dp 준비
-            dp[i]=i+dp[i-1]-1;
+        int ct=1;
+        L:for(int i=1;;i++){
+            for(int j=1,k=i;j<=i;j++,k--){
+                xa[ct]=j;
+                ya[ct]=k;
+                if(++ct==ag) break L;
+            }
         }
         int t=Integer.parseInt(I.readLine());
         while(t-->0){
             StringTokenizer st=new StringTokenizer(I.readLine());
             int a=Integer.parseInt(st.nextToken()),b=Integer.parseInt(st.nextToken());
-            x=0; y=0;
-            fc(a); fc(b);
-            as.append(dp[x+y]-y+1).append('\n');
+            int x=xa[a]+xa[b],y=ya[a]+ya[b];
+            int n=x+y;
+            as.append((n*n-3*n)/2+x+1).append('\n');
         }
         System.out.println(as);
     }
