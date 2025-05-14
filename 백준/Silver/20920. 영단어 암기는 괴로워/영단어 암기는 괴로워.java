217253db2bad90334379b1ec478f6ed57f4b9a19
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-14 14:25:20 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 740 ms, Memory: 48080 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -7,51 +7,29 @@ class Main{
 
         StringTokenizer st=new StringTokenizer(I.readLine());
         int n=Integer.parseInt(st.nextToken()),m=Integer.parseInt(st.nextToken());
-
-        String[] ip=new String[n];
         HashMap<String,Integer> hs=new HashMap<>();
-        int[][] info=new int[n+1][3];
-        int sz=0;
 
         for(int i=0;i<n;i++){
             String s=I.readLine();
-            int l=s.length();
+            int l=s.length(),ct=0;
             if(l<m) continue;
-            if(hs.containsKey(s)){
-                int id=hs.get(s);
-                info[id][0]++;
-            }else{
-                hs.put(s,sz);
-                info[sz][0]++;
-                info[sz][1]=l;
-                info[sz][2]=sz;
-                ip[sz++]=s;
-            }
+            if(hs.containsKey(s)) ct=hs.get(s);
+            hs.put(s,1+ct);
         }
 
-        Arrays.sort(info,(a,b)->Integer.compare(b[0],a[0]));
+        List<Map.Entry<String,Integer>> wd=new ArrayList<>(hs.entrySet());
 
-        int id=0;
-        while(id<sz){
-            int mx=info[id][0];
-            ArrayList<int[]>arc=new ArrayList<>();
-            while(id<sz){
-                if(mx==info[id][0]){
-                    arc.add(new int[]{info[id][1],info[id][2]});
-                    id++;
-                }else break;
-            }
-            Collections.sort(arc,(a,b)->Integer.compare(b[0],a[0]));
-            for(int i=10;i>0;i--){
-                ArrayList<String> op=new ArrayList<>();
-                for(int[] p:arc){
-                    int c=p[0],dx=p[1];
-                    if(c==i) op.add(ip[dx]);
-                }
-                Collections.sort(op);
-                for(String s:op) O.write(s+'\n');
-            }
-        }        
+        wd.sort((a,b)->{
+            int c=Integer.compare(b.getValue(),a.getValue());
+            if(c==0) c=Integer.compare(b.getKey().length(),a.getKey().length());
+            if(c==0) c=a.getKey().compareTo(b.getKey());
+            return c;
+        });
+
+        for(Map.Entry<String,Integer> e:wd){
+            O.write(e.getKey()+'\n');
+        }
+
         O.flush();
     }
 }
