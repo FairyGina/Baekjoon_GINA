248715d8a78b4c01f4a798b55755c173b691e8da
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-15 03:24:23 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 200 ms, Memory: 78288 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -5,40 +5,41 @@ class Main{
     static StringBuilder sp=new StringBuilder("   ");
     static void mkst(int ip){
         if(ip>n) return;
-        StringBuilder[] make=new StringBuilder[ip];
         int ct=ip/3,id=-1;
+        String[] t=new String[ct];
+        for(int i=0;i<ct;i++){
+            t[i]=String.valueOf(star[i]);
+        }
         int i=-1;
         while(++i<ct){
-            make[++id]=new StringBuilder(star[i]);
-            make[id].append(star[i]).append(star[i]);
+            star[++id]=new StringBuilder(t[i]);
+            star[id].append(t[i]).append(t[i]);
         }
         i=-1;
         while(++i<ct){
-            make[++id]=new StringBuilder(star[i]);
-            make[id].append(sp).append(star[i]);
+            star[++id]=new StringBuilder(t[i]);
+            star[id].append(sp).append(t[i]);
         }
         i=-1;
         while(++i<ct){
-            make[++id]=make[i];
+            star[++id]=star[i];
         }
         String ts=String.valueOf(sp);
         sp.append(ts).append(ts);
-        star=make;
         mkst(ip*3);
     }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder as=new StringBuilder();
+        BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
         n=Integer.parseInt(I.readLine());
-        star=new StringBuilder[3];
+        star=new StringBuilder[n];
         star[0]=new StringBuilder("***");
         star[1]=new StringBuilder("* *");
         star[2]=new StringBuilder("***");
         mkst(9);
         for(int i=0;i<n;i++){
-            as.append(star[i]).append('\n');
+            O.write(star[i]+"\n");
         }
-        System.out.println(as);
-
+        O.flush();
     }
 }
