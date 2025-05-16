e1bc94c7a9f496cc631afdfb271bd4a9c57dbdba
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-17 06:28:14 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 12128 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -3,62 +3,53 @@ import java.util.*;
 
 class Main{
     static int n,max=-1000000000,min=1000000000;
-    static boolean[] vt;
-    static int[] oper,nb,op;
-    static int cc(int a,int b,int c){
-        switch(c){
-            case 0:
-                return a+b;
-            case 1:
-                return a-b;
-            case 2:
-                return a*b;
-            default:
-                return a/b;
+    static int[] oper,nb;
+    static void cal(int id,int cur){
+        if(id==n){
+            if(cur<min) min=cur;
+            if(cur>max) max=cur;
+            return;
         }
-    }
-    static void cal(int id){
-        if(id==n-1){
-            int res=nb[0];
-            for(int i=0;i<id;i++){
-                res=cc(res,nb[i+1],op[i]);
+        for(int i=0;i<4;i++){
+            if(oper[i]>0){
+                oper[i]--;
+                int res=cur;
+                switch(i){
+                    case 0:
+                        res+=nb[id];
+                        break;
+                    case 1:
+                        res-=nb[id];
+                        break;
+                    case 2:
+                        res*=nb[id];
+                        break;
+                    default:
+                        res/=nb[id];
+                        break;
+                }
+                cal(id+1,res);
+                oper[i]++;
             }
-            if(res<min) min=res;
-            if(res>max) max=res;
-        }
-        for(int i=0;i<n-1;i++){
-            if(!vt[i]){
-                vt[i]=true;
-                op[id]=oper[i];
-                cal(id+1);
-                vt[i]=false;
-            }
-        }
-    }
-    static void opera(int st,int ct,int n){
-        for(int i=0;i<ct;i++){
-            oper[i+st]=n;
         }
     }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+
         n=Integer.parseInt(I.readLine());
-        vt=new boolean[n-1];
-        op=new int[n-1];
-        oper=new int[n-1];
         nb=new int[n];
         StringTokenizer st=new StringTokenizer(I.readLine());
         for(int i=0;i<n;i++){
             nb[i]=Integer.parseInt(st.nextToken());
         }
-        int ta=0;
+
+        oper=new int[4];
         st=new StringTokenizer(I.readLine());
         for(int i=0;i<4;i++){
-            int tm=Integer.parseInt(st.nextToken());
-            opera(ta,tm,i);
-            ta+=tm;
+            oper[i]=Integer.parseInt(st.nextToken());
         }
-        cal(0);
+
+        cal(1,nb[0]);
         System.out.println(max+"\n"+min);
     }
 }
