5a3373ba5a7f7e67e4bd11689328bc7867b21f6b
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-16 15:54:16 +0900

    [Silver III] Title: N과 M (3), Time: 232 ms, Memory: 108992 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -3,27 +3,26 @@ import java.util.*;
 class Main{
     static StringBuilder as=new StringBuilder();
     static int n,m;
-    static int[] nb;
+    static char[] nb;
     static void seq(int id){
         if(id==m){
-            for(int i=0;i<m;i++){
-                as.append(nb[i]).append(' ');
-            }
-            as.append('\n');
+            as.append(nb);
             return;
         }
         for(int i=1;i<=n;i++){
-            nb[id]=i;
+            nb[id*2]=(char)(i+'0');
             seq(id+1);
         }
     }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        
         StringTokenizer st=new StringTokenizer(I.readLine());
         n=Integer.parseInt(st.nextToken()); m=Integer.parseInt(st.nextToken());
-        nb=new int[m];
-        
+        nb=new char[m*2];
+        for(int i=0;i<m-1;i++){
+            nb[2*i+1]=' ';
+        }
+        nb[2*m-1]='\n';
         seq(0);
         System.out.println(as);
     }
