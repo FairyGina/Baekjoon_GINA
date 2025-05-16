50fd8668ee5dc491f4f2eff33ee91c29a17c93cd
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-16 16:03:17 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,20 +3,17 @@ import java.util.*;
 class Main{
     static StringBuilder as=new StringBuilder();
     static int n,m;
-    static int[] nb;
+    static char[] nb;
     static boolean[] vt;
     static void seq(int id){
         if(id==m){
-            for(int i=0;i<m;i++){
-                as.append(nb[i]).append(' ');
-            }
-            as.append('\n');
+            as.append(nb);
             return;
         }
         for(int i=1;i<=n;i++){
             if(!vt[i]){
                 vt[i]=true;
-                nb[id]=i;
+                nb[id*2]=(char)(i+'0');
                 seq(id+1);
                 vt[i]=false;
             }
@@ -26,8 +23,12 @@ class Main{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(I.readLine());
         n=Integer.parseInt(st.nextToken()); m=Integer.parseInt(st.nextToken());
-        nb=new int[m];
+        nb=new char[m*2];
         vt=new boolean[n+1];
+        for(int i=0;i<m-1;i++){
+            nb[2*i+1]=' ';
+        }
+        nb[2*m-1]='\n';
         seq(0);
         System.out.println(as);
     }
