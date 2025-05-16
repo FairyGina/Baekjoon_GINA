aa59264eb1f08a488c521a0883d3b744a0e27988
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-16 15:31:45 +0900

    [Silver III] Title: N과 M (2), Time: 64 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,8 +4,6 @@ class Main{
     static StringBuilder as=new StringBuilder();
     static int n,m;
     static int[] nb;
-    static boolean[] vt;
-
     static void seq(int id,int p){
         if(id==m){
             for(int i=0;i<m;i++){
@@ -14,25 +12,18 @@ class Main{
             as.append('\n');
             return;
         }
-        for(int i=1+p;i<=n;i++){
-            if(!vt[i]){
-                vt[i]=true;
-                nb[id]=i;
-                seq(id+1,i);
-                vt[i]=false;
-            }
+        for(int i=p;i<=n;i++){
+            nb[id]=i;
+            seq(id+1,i+1);
         }
     }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        
         StringTokenizer st=new StringTokenizer(I.readLine());
         n=Integer.parseInt(st.nextToken()); m=Integer.parseInt(st.nextToken());
         nb=new int[m];
-        vt=new boolean[n+1];
         
-        seq(0,0);
-    
+        seq(0,1);
         System.out.println(as);
     }
 }
