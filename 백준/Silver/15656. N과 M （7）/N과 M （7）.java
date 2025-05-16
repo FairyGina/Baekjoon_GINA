43ffbb5f5a999403f2b1c36d7cd20666f8cc1ac3
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-16 16:52:18 +0900

    [Silver III] Title: N과 M (7), Time: 504 ms, Memory: 219772 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    static StringBuilder as=new StringBuilder();
+    static int n,m;
+    static int[] nb;
+    static int[] ip;
+
+    static void seq(int id){
+        if(id==m){
+            for(int i=0;i<m;i++){
+                as.append(nb[i]).append(' ');
+            }
+            as.append('\n');
+            return;
+        }
+        for(int i=0;i<n;i++){
+            nb[id]=ip[i];
+            seq(id+1);
+        }
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st=new StringTokenizer(I.readLine());
+        n=Integer.parseInt(st.nextToken()); m=Integer.parseInt(st.nextToken());
+
+        ip=new int[n];
+        st=new StringTokenizer(I.readLine());
+        for(int i=0;i<n;i++){
+            ip[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(ip);
+        nb=new int[n];
+
+        seq(0);
+        System.out.println(as);
+    }
+}
