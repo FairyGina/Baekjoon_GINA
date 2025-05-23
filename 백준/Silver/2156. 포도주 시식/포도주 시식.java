50e65cdc3b51029fc81307c203d3c462ae05ed2c
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-24 00:58:33 +0900

    [Silver I] Title: 포도주 시식, Time: 92 ms, Memory: 13368 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -2,22 +2,29 @@ import java.io.*;
 class Main{
         public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+
         int n=Integer.parseInt(I.readLine());
-        int[][] dp=new int[n][3];
-        for(int i=0;i<n;i++){
-            dp[i][0]=Integer.parseInt(I.readLine());
+        int[] gp=new int[n+1];
+        int[] dp=new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            gp[i]=Integer.parseInt(I.readLine());
         }
-        int max=dp[0][0];
-        if(n>1){
-            dp[0][1]=dp[0][2]=max;
-            dp[1][1]=max+dp[1][0];
-            dp[1][2]=dp[1][0];
+        if(n==1){
+            System.out.print(gp[1]);
+            return;
+        }else if(n==2){
+            System.out.print(gp[1]+gp[2]);
+            return;
         }
-        for(int i=2;i<n;i++){
-            dp[i][1]=dp[i][0]+dp[i-1][2];
-            dp[i][2]=dp[i][0]+max;
-            max=Math.max(max,Math.max(dp[i-1][1],dp[i-1][2]));
+
+        dp[1]=gp[1];
+        dp[2]=gp[1]+gp[2];
+        for(int i=3;i<=n;i++){
+            dp[i]=Math.max(dp[i-3]+gp[i-1],dp[i-2]);
+            dp[i]+=gp[i];
+            dp[i]=Math.max(dp[i],dp[i-1]);
         }
-        System.out.print(Math.max(max,(Math.max(dp[n-1][1],dp[n-1][2]))));
+        System.out.print(dp[n]);
     }
 }
