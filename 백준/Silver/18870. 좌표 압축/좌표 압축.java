b6b003052c19b73d31bf1582428dbb4647d57568
GINA <101798990+FairyGina@users.noreply.github.com>
2025-05-28 02:46:10 +0900

    [Silver II] Title: 좌표 압축, Time: 1644 ms, Memory: 367276 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,32 +1,28 @@
 import java.io.*;
 import java.util.*;
 class Main{
-    static int[] arr;
-    public static int bs(int s){
-        int l=0,r=arr.length;
-        while(l<=r){
-            int m=(l+r)/2;
-            if(arr[m]<s) l=m+1;
-            else if(arr[m]>s) r=m-1;
-            else return m;
-        }
-        return 0;
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter O=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder as=new StringBuilder();
         int n=Integer.parseInt(I.readLine());
-        int[] num=new int[n];
-        Set<Integer> a=new TreeSet<>();
-        StringTokenizer s=new StringTokenizer(I.readLine());
+        int[] nb=new int[n],mk=new int[n];
+        StringTokenizer st=new StringTokenizer(I.readLine());
         for(int i=0;i<n;i++){
-            int t=Integer.parseInt(s.nextToken());
-            num[i]=t;
-            a.add(t);
+            nb[i]=Integer.parseInt(st.nextToken());
         }
-        arr=a.stream().mapToInt(Integer::intValue).toArray();
-        for(int i=0;i<n;i++) O.write(bs(num[i])+" ");
-        O.write("\n");
-        O.flush();
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        System.arraycopy(nb, 0, mk, 0, n);
+        Arrays.sort(mk);
+        int id=0;
+        for(int i=0;i<n;i++){
+            if(!hm.containsKey(mk[i])){
+                hm.put(mk[i],id++);
+            }
+        }
+        for(int i=0;i<n;i++){
+            int tp=hm.get(nb[i]);
+            as.append(tp).append(' ');
+        }
+        System.out.print(as);
     }
 }
