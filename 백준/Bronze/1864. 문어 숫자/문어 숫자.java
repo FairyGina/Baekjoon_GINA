9217e957022b5e3114c71828eca9f45f00eddcc5
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-01 02:49:43 +0900

    [Bronze II] Title: 문어 숫자, Time: 68 ms, Memory: 11336 KB -BaekjoonHub

--- 백준/Bronze/1864. 문어 숫자/문어 숫자.java
+++ 백준/Bronze/1864. 문어 숫자/문어 숫자.java
@@ -1,26 +1,25 @@
 import java.io.*;
-import java.util.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder as=new StringBuilder();
-        HashMap<Character,Integer> hm=new HashMap<>();
-        hm.put('-',0);
-        hm.put('\\',1);
-        hm.put('(',2);
-        hm.put('@',3);
-        hm.put('?',4);
-        hm.put('>',5);
-        hm.put('&',6);
-        hm.put('%',7);
-        hm.put('/',-1);
+        int[] hm=new int[93];
+        hm['-']=0;
+		hm['\\']=1;
+		hm['(']=2;
+		hm['@']=3;
+		hm['?']=4;
+		hm['>']=5;
+		hm['&']=6;
+		hm['%']=7;
+		hm['/']=-1;
         String s;
         while(!(s=I.readLine()).equals("#")){
             long res=0;
             int len=s.length();
             int x=len-1;
             for(int i=0;i<len;i++){
-                res+=Math.pow(8,x--)*hm.get(s.charAt(i));
+                res+=Math.pow(8,x--)*hm[(s.charAt(i))];
             }
             as.append(res).append('\n');
         }
