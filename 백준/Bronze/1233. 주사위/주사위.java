bab6be7f65b0b09c2c5a0b461493a0276cd651cc
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-01 14:46:12 +0900

    [Bronze II] Title: 주사위, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/1233. 주사위/주사위.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(I.readLine());
+        int a=Integer.parseInt(st.nextToken()),b=Integer.parseInt(st.nextToken()),c=Integer.parseInt(st.nextToken());
+        
+        int[] ct=new int[121];
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                for(int k=1;k<=c;k++){
+                    ct[i+j+k]++;
+                }
+            }
+        }
+        int max=0,id=0;
+        for(int i=3;i<121;i++){
+            if(ct[i]>max){
+                max=ct[i];
+                id=i;
+            }
+        }
+        System.out.print(id);
+    }
+}
