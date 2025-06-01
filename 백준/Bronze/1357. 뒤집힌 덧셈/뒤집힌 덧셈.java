c4e83a98296f64aa58bf803e99db8c7fdbca385a
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-01 19:48:52 +0900

    [Bronze II] Title: 뒤집힌 덧셈, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.java
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.java
@@ -1,17 +1,13 @@
 import java.io.*;
 import java.util.*;
 class Main{
-    static int rv(String s){
-        StringBuilder sb=new StringBuilder(s).reverse();
-        int id=-1,len=s.length();
-        while(id<len){
-            if(sb.charAt(++id)!='0') break;
-        }
-        return Integer.parseInt(sb.substring(id,len));
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(I.readLine());
-        System.out.print(rv(String.valueOf(rv(st.nextToken())+rv(st.nextToken()))));
+        StringBuilder a=new StringBuilder(st.nextToken()).reverse(),
+        b=new StringBuilder(st.nextToken()).reverse();
+        int sum=Integer.parseInt(String.valueOf(a))+Integer.parseInt(String.valueOf(b));
+        a=new StringBuilder(String.valueOf(sum)).reverse();
+        System.out.print(Integer.parseInt(String.valueOf(a)));
     }
 }
