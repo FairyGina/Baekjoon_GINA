e4f909b9664bf959f1d750ca106cf41e90af4f75
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-04 02:42:53 +0900

    [Silver III] Title: 수 복원하기, Time: 96 ms, Memory: 12224 KB -BaekjoonHub

--- 백준/Silver/2312. 수 복원하기/수 복원하기.java
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.java
@@ -1,8 +1,7 @@
 import java.io.*;
 class Main{
-    static int lm=100001,plm=9592;
-    static boolean[] prime=new boolean[100001];
-    static int[] pnb=new int[plm];
+    static int lm=100001;
+    static boolean[] prime=new boolean[lm];
     static void pri(){
         prime[0]=prime[1]=true;
         for(int i=2;i*i<100001;i++){
@@ -13,32 +12,26 @@ class Main{
             }
         }
     }
-    static void insnb(){
-        int id=0;
-        for(int i=2;i<100001;i++){
-            if(!prime[i]){
-                pnb[id++]=i;
-            }
-        }
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        pri(); insnb();
+        pri();
         StringBuilder as=new StringBuilder();
         int t=Integer.parseInt(I.readLine());
         while(t-->0){
             int ct=0;
             int n=Integer.parseInt(I.readLine());
-            for(int i=0;i<plm;i++){
-                if(n%pnb[i]==0){
-                    ct++;
-                    n/=pnb[i];
-                    i--;
-                }else{
-                    if(ct>0){
-                        as.append(String.format("%d %d\n",pnb[i],ct));
-                        ct=0;
-                    }if(n==1) break;
+            for(int i=0;i<lm;i++){
+                if(!prime[i]){
+                    if(n%i==0){
+                        ct++;
+                        n/=i;
+                        i--;
+                    }else{
+                        if(ct>0){
+                            as.append(i).append(' ').append(ct).append('\n');
+                            ct=0;
+                        }if(n==1) break;
+                    }
                 }
             }
         }
