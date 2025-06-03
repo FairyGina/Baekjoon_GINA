d0276cf87af37526f973db7d2ab7eff012d4f073
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-04 02:55:40 +0900

    [Silver III] Title: 수 복원하기, Time: 68 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Silver/2312. 수 복원하기/수 복원하기.java
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.java
@@ -1,38 +1,23 @@
 import java.io.*;
 class Main{
-    static int lm=100001;
-    static boolean[] prime=new boolean[lm];
-    static void pri(){
-        prime[0]=prime[1]=true;
-        for(int i=2;i*i<100001;i++){
-            if(!prime[i]){
-                for(int j=i*i;j<100001;j+=i){
-                    prime[j]=true;
-                }
-            }
-        }
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        pri();
         StringBuilder as=new StringBuilder();
         int t=Integer.parseInt(I.readLine());
         while(t-->0){
-            int ct=0;
             int n=Integer.parseInt(I.readLine());
-            for(int i=0;i<lm;i++){
-                if(!prime[i]){
-                    if(n%i==0){
-                        ct++;
-                        n/=i;
-                        i--;
-                    }else{
-                        if(ct>0){
-                            as.append(i).append(' ').append(ct).append('\n');
-                            ct=0;
-                        }if(n==1) break;
-                    }
+            for(int i=2;i*i<=n;i++){
+                int ct=0;
+                while(n%i==0){
+                    ct++;
+                    n/=i;
                 }
+                if(ct>0){
+                    as.append(i).append(' ').append(ct).append('\n');
+                }   
+            }
+            if(n>1){
+                as.append(n).append(' ').append(1).append('\n');
             }
         }
         System.out.print(as);
