a16526cf09ff31001b989355b3b5d72fc57fb711
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-03 22:51:22 +0900

    [Bronze II] Title: 좋은 자동차 번호판, Time: 72 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Bronze/1871. 좋은 자동차 번호판/좋은 자동차 번호판.java
+++ 백준/Bronze/1871. 좋은 자동차 번호판/좋은 자동차 번호판.java
@@ -1,16 +1,14 @@
 import java.io.*;
 import java.util.StringTokenizer;
 class Main{
-    static boolean ck(String s,String b){
-        int f=(s.charAt(0)-65)*26*26+(s.charAt(1)-65)*26+(s.charAt(2)-65);
-        return Math.abs(f-Integer.parseInt(b))<101;
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(I.readLine());
         while(n-->0){
             StringTokenizer st=new StringTokenizer(I.readLine(),"-");
-            System.out.println(ck(st.nextToken(),st.nextToken())?"nice":"not nice");
+            String s=st.nextToken();
+            int f=(s.charAt(0)-65)*26*26+(s.charAt(1)-65)*26+(s.charAt(2)-65);
+            System.out.println(Math.abs(f-Integer.parseInt(st.nextToken()))<101?"nice":"not nice");
         }
     }
 }
