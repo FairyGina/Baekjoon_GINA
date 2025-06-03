32db89a827b624b9f89c0a0b97664df06f3cb6a3
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-03 23:31:11 +0900

    [Bronze II] Title: 완전제곱수, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.java
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.java
@@ -1,31 +1,27 @@
 import java.io.*;
 class Main{
-    static int m,n,r=0,mn;
-    static void fd(int i){
-        if(i*i>=m){
-            m=i;
-            mn=i*i;
-            return;
-        }
-        fd(++i);
-    }
-    static void sc(int i){
-        int tr=i*i;
-        if(tr>n) return;
-        r+=tr;
-        sc(++i);
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        m=Integer.parseInt(I.readLine());
-        fd(1);
-        n=Integer.parseInt(I.readLine());
-        sc(m);
+        int m=Integer.parseInt(I.readLine()),n=Integer.parseInt(I.readLine());
+        int i=1,r=0;
+        while(true){
+            if(i*i>=m){
+                m=i*i;
+                break;
+            }
+            i++;
+        }
+        while(true){
+            int tr=i*i;
+            if(tr>n) break;
+            r+=tr;
+            i++;
+        }
         if(r==0){
             System.out.print(-1);
             return;
         }
         System.out.println(r);
-        System.out.print(mn);
+        System.out.print(m);
     }
 }
