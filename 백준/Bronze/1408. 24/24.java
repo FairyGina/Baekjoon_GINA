1899cbf49c4f173720f8ce2c99a345f3fb4b600e
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-03 17:46:21 +0900

    [Bronze II] Title: 24, Time: 68 ms, Memory: 11780 KB -BaekjoonHub

--- 백준/Bronze/1408. 24/24.java
+++ 백준/Bronze/1408. 24/24.java
@@ -1,32 +1,22 @@
 import java.io.*;
+import java.util.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        int[] start=new int[3],cur=new int[3];
-        String s=I.readLine();
-        String[] ts=s.split(":");
-        int sec=-1;
-        while(++sec<3){
-            cur[sec]=Integer.parseInt(ts[sec]);
-        }
-        s=I.readLine();
-        ts=s.split(":");
-        sec=-1;
-        while(++sec<3){
-            start[sec]=Integer.parseInt(ts[sec]);
-        }
-        sec=start[2]-cur[2];
-        if(sec<0){
-            sec+=60;
-            cur[1]++;
-        }
-        int min=start[1]-cur[1];
-        if(min<0){
-            min+=60;
-            cur[0]++;
-        }
-        int ho=start[0]-cur[0];
-        if(ho<0) ho+=24;
-        System.out.printf("%02d:%02d:%02d",ho,min,sec);
+        
+        StringTokenizer st=new StringTokenizer(I.readLine(),":");
+        int cur=Integer.parseInt(st.nextToken())*3600+Integer.parseInt(st.nextToken())*60+Integer.parseInt(st.nextToken());
+
+        st=new StringTokenizer(I.readLine(),":");
+        int mis=Integer.parseInt(st.nextToken())*3600+Integer.parseInt(st.nextToken())*60+Integer.parseInt(st.nextToken());
+
+        int ans=mis-cur;
+
+        if(ans<0) ans+=86400;
+        int h=ans/3600;
+        ans%=3600;
+        int m=ans/60;
+        ans%=60;
+        System.out.printf("%02d:%02d:%02d",h,m,ans);
     }
 }
