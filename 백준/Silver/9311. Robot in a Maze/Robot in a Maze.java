aeacaca6d53ac58ef2cc9b9ca15233a6c0db4f86
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-06 04:21:21 +0900

    [Silver III] Title: Robot in a Maze, Time: 60 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/9311. Robot in a Maze/Robot in a Maze.java
+++ 백준/Silver/9311. Robot in a Maze/Robot in a Maze.java
@@ -3,23 +3,25 @@ import java.util.*;
 class Main{
     static int n,m,sh=0;
     static int[] ya={-1,0,1,0},xa={0,1,0,-1};
-    static int[][] count;
     static char[][] miro;
     static Deque<Integer> q=new ArrayDeque<>();
     static boolean find(){
+        int dt=0;
         while(!q.isEmpty()){
-            int x=q.poll(),y=q.poll();
-            int ct=count[y][x]+1;
-            for(int i=0;i<4;i++){
-                int tx=x+xa[i],ty=y+ya[i];
-                if(-1<tx&&tx<m&&-1<ty&&ty<n&&miro[ty][tx]!='X'){
-                    count[ty][tx]=ct;
-                    if(miro[ty][tx]=='G'){
-                        sh=ct;
-                        return true;
+            int sz=q.size()/2;
+            dt++;
+            while(sz-->0){
+                int x=q.poll(),y=q.poll();
+                for(int i=0;i<4;i++){
+                    int tx=x+xa[i],ty=y+ya[i];
+                    if(-1<tx&&tx<m&&-1<ty&&ty<n&&miro[ty][tx]!='X'){
+                        if(miro[ty][tx]=='G'){
+                            sh=dt;
+                            return true;
+                        }
+                        q.offer(tx); q.offer(ty);
+                        miro[ty][tx]='X';
                     }
-                    q.offer(tx); q.offer(ty);
-                    miro[ty][tx]='X';
                 }
             }
         }
@@ -34,7 +36,6 @@ class Main{
             n=Integer.parseInt(st.nextToken()); m=Integer.parseInt(st.nextToken());
             q.clear();
             miro=new char[n][m];
-            count=new int[n][m];
             for(int i=0;i<n;i++){
                 String ts=I.readLine();
                 int id=ts.indexOf('S');
