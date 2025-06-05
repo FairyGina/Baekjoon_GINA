acfec8e56bc9fce3934a30961eb69bab5e0f7ac7
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-06 02:40:26 +0900

    [Silver IV] Title: 세상은 하나의 손수건, Time: 260 ms, Memory: 38608 KB -BaekjoonHub

--- 백준/Silver/20493. 세상은 하나의 손수건/세상은 하나의 손수건.java
+++ 백준/Silver/20493. 세상은 하나의 손수건/세상은 하나의 손수건.java
@@ -1,49 +1,40 @@
 import java.io.*;
 import java.util.*;
 class Main{
+    static int x=0,y=0;
+    static void mv(int id,int m){
+        switch(id){
+            case 0:
+                x+=m;
+                break;
+            case 1:
+                y-=m;
+                break;
+            case 2:
+                x-=m;
+                break;
+            case 3:
+                y+=m;
+                break;
+        }
+    }
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(I.readLine());
+        
         int ti=Integer.parseInt(st.nextToken()),t=Integer.parseInt(st.nextToken());
-        int id=0,save=0,x=0,y=0,move;
+        int id=0,save=0,move;
         while(ti-->0){
             st=new StringTokenizer(I.readLine());
             int n=Integer.parseInt(st.nextToken());
             move=n-save;
             save=n;
-            switch(id){
-                case 0:
-                    x+=move;
-                    break;
-                case 1:
-                    y-=move;
-                    break;
-                case 2:
-                    x-=move;
-                    break;
-                case 3:
-                    y+=move;
-                    break;
-            }
-
+            mv(id,move);
             if(st.nextToken().equals("right")) id=(id+1)%4;
             else id=(id+3)%4;
         }
         move=t-save;
-        switch(id){
-            case 0:
-                x+=move;
-                break;
-            case 1:
-                y-=move;
-                break;
-            case 2:
-                x-=move;
-                break;
-            case 3:
-                y+=move;
-                break;
-        }
+        mv(id,move);
         System.out.print(x+" "+y);
     }
 }
