921ce7740685d43a399529a8ead3ecb65d5ba5eb
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-06 01:15:10 +0900

    [Bronze I] Title: 나무 조각, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder as=new StringBuilder();
+        String s=I.readLine();
+        char[] arr=new char[10];
+        for(int i=0;i<9;i+=2){
+            arr[i]=s.charAt(i);
+            arr[i+1]=' ';
+        }
+        for(int t=0;t<4;t++){
+            int lm=7-t*2;
+            for(int i=0;i<lm;i+=2){
+                if((int)arr[i]>(int)arr[i+2]){
+                    char c=arr[i];
+                    arr[i]=arr[i+2];
+                    arr[i+2]=c;
+                    as.append(arr).append('\n');
+                }
+            }
+        }
+        System.out.print(as);
+    }
+}
