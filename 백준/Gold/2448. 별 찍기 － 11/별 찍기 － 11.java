445798605afa13bbb059e805be7c884af45cc488
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-05 17:50:18 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 316 ms, Memory: 200200 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -3,20 +3,19 @@ class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder as=new StringBuilder();
+        
         int n=Integer.parseInt(I.readLine());
+        
         StringBuilder[] star=new StringBuilder[n];
-        for(int i=0;i<n;i++){
-            star[i]=new StringBuilder();
-        }
         StringBuilder space=new StringBuilder("     ");
-        star[0].append("*");
-        star[1].append("* *");
-        star[2].append("*****");
+        star[0]=new StringBuilder("*");
+        star[1]=new StringBuilder("* *");
+        star[2]=new StringBuilder("*****");
+        
         for(int t=3;t<n;t*=2){
             StringBuilder copy=new StringBuilder(space);
             for(int j=0;j<t;j++){
-                star[j+t].append(star[j]).append(copy);
-                int sz=copy.length();
+                star[j+t]=new StringBuilder(star[j]).append(copy);
                 copy.delete(0,2);
             }
             for(int j=0;j<t;j++){
@@ -26,7 +25,7 @@ class Main{
         }
         space.delete(0,space.length()/2+1);
         for(int i=0;i<n;i++){
-            as.append(space).append(star[i]).append(space).append(' ').append('\n');
+            as.append(space).append(star[i]).append(space).append('\n');
             space.delete(0,1);
         }
         System.out.print(as);
