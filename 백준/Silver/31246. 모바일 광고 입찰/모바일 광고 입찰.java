f509d1647213d1d5c09198ac5795904e8d607db4
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-14 02:44:42 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 336 ms, Memory: 44904 KB -BaekjoonHub

--- 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
@@ -3,14 +3,12 @@ import java.io.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        
         StringTokenizer st=new StringTokenizer(I.readLine());
         int n=Integer.parseInt(st.nextToken()),k=Integer.parseInt(st.nextToken())-1;
         int[] nb=new int[n];
         for(int i=0;i<n;i++){
             st=new StringTokenizer(I.readLine());
-            int c=Integer.parseInt(st.nextToken())-Integer.parseInt(st.nextToken());
-            nb[i]=-c;
+            nb[i]=-Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken());
         }
         Arrays.sort(nb);
         System.out.print(nb[k]<1?0:nb[k]);
