9a683597aa83e8aaa5789ee11e2f6831694a49d2
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-25 02:24:48 +0900

    [Silver II] Title: COW, Time: 80 ms, Memory: 13956 KB -BaekjoonHub

--- 백준/Silver/10751. COW/COW.java
+++ 백준/Silver/10751. COW/COW.java
@@ -1,37 +1,19 @@
 import java.io.*;
-//import java.util.*;
 class Main{
     static boolean bl=true;
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
 
-        long res=0;
-        int len=Integer.parseInt(I.readLine());
+        long[] ct=new long[3];
+        int n=Integer.parseInt(I.readLine());
         String s=I.readLine();
-        int[] cn=new int[len+1],on=new int[len+1],wn=new int[len+1];
-        for(int i=0;i<len;i++){
-            cn[i+1]=cn[i];
-            on[i+1]=on[i];
-            wn[i+1]=wn[i];
-            if(s.charAt(i)=='C') cn[i+1]++;
-            else if(s.charAt(i)=='O') on[i+1]++;
-            else wn[i+1]++;
+
+        for(int i=0;i<n;i++){
+            char c=s.charAt(i);
+            if(c=='C') ct[0]++;
+            else if(c=='O') ct[1]+=ct[0];
+            else ct[2]+=ct[1];
         }
-        int[] ores=new int[len+1];
-        for(int i=1;i<=len;i++){
-            ores[i]=ores[i-1];
-            if(on[i]>on[i-1]){
-                ores[i]+=wn[len]-wn[i];
-            }
-        }
-        long tp=0;
-        for(int i=1;i<=len;i++){
-            res=tp;
-            if(cn[i]>cn[i-1]){
-                res+=ores[len]-ores[i];
-            }
-            tp=res;
-        }
-        System.out.print(res);
+        System.out.print(ct[2]);
     }
 }
