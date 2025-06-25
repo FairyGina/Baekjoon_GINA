10643ad976e1b06a60b5f17dca20bebd3bee5f8e
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-26 01:49:49 +0900

    [Bronze II] Title: Crazy Fencing, Time: 116 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Bronze/21617. Crazy Fencing/Crazy Fencing.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder as=new StringBuilder();
+        double res=0;
+        int n=Integer.parseInt(I.readLine());
+        int[] h=new int[n+1];
+        StringTokenizer st=new StringTokenizer(I.readLine());
+        for(int i=0;i<=n;i++){
+            h[i]=Integer.parseInt(st.nextToken());
+        }
+        st=new StringTokenizer(I.readLine());
+        for(int i=0;i<n;i++){
+            res+=(double)((h[i]+h[i+1])*Integer.parseInt(st.nextToken()))/2;
+        }
+        if(res==(long)res) System.out.printf("%.0f",res);
+        else System.out.print(res);
+    }
+}
