531714b1e059fc0737e9ac56d059306961e77d40
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-30 03:35:49 +0900

    [Bronze II] Title: 팰린드롬, Time: 64 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/10174. 팰린드롬/팰린드롬.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder as=new StringBuilder();
+        int t=Integer.parseInt(I.readLine());
+        while(t-->0){
+            String s=I.readLine().toUpperCase(),ans="Yes\n";
+            int lt=s.length(),r=lt/2,l=r-1;
+            if(lt==1) ans="No\n";
+            if(lt%2!=0) r++;
+            while(l>=0){
+                if(s.charAt(l--)!=s.charAt(r++)){
+                    ans="No\n";
+                    break;
+                }
+            }
+            as.append(ans);
+        }
+        System.out.print(as);
+    }
+}
