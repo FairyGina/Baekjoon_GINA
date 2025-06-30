6af0c78bdbc3408975da5307232d8e58f0656879
GINA <101798990+FairyGina@users.noreply.github.com>
2025-06-30 22:48:26 +0900

    [Silver IV] Title: 싱크홀, Time: 72 ms, Memory: 11616 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/15830. 싱크홀/싱크홀.java
+++ 백준/Silver/15830. 싱크홀/싱크홀.java
@@ -4,13 +4,11 @@ class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(I.readLine());
-
         double v=Double.parseDouble(st.nextToken()),w=Double.parseDouble(st.nextToken()),d=Double.parseDouble(st.nextToken());
         int ct=-1;
         while(d>0){
-            double t=w/v;
-            double s=5*t*t;
-            v*=0.8;
+            double s=5*w*w/(v*v);
+            v=v*8/10;
             d-=s;
             ct++;
         }
