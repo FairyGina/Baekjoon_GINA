4d1ca9d7311e62feb0620976a0824b3d2a607895
GINA <101798990+FairyGina@users.noreply.github.com>
2025-07-12 21:16:24 +0900

    [Bronze IV] Title: 인간은 무엇인가, Time: 68 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/32651. 인간은 무엇인가/인간은 무엇인가.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(I.readLine());
+        System.out.print((n<100001&&n%2024==0)?"Yes":"No");
+    }
+}
