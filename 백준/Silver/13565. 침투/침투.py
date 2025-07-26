cd0ffb2c0fa28ee109ba35f2e67cc9907e79e7ab
GINA <101798990+FairyGina@users.noreply.github.com>
2025-07-26 12:55:31 +0900

    [Silver II] Title: 침투, Time: 216 ms, Memory: 50996 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.py
+++ 백준/Silver/13565. 침투/침투.py
@@ -1,36 +1,26 @@
 import sys
-import copy
-from collections import deque
+sys.setrecursionlimit(10**6)
 def input(): return sys.stdin.readline().rstrip()
 my=(-1,0,1,0)
 mx=(0,1,0,-1)
-q=deque()
-def per():
-    while q:
-        y=q.popleft()
-        x=q.popleft()
-        if y==m-1: return True
-        for i in range(4):
-            ty=y+my[i]
-            tx=x+mx[i]
-            if 0<=ty<m and 0<=tx<n and not board[ty][tx]:
-                q.append(ty)
-                q.append(tx)
-                board[ty][tx]=1
-    return False
-    
+ans="NO"
 m,n=map(int,input().split())
 board=[list(map(int,input())) for _ in range(m)]
+def per(y,x):
+    if y==m-1:
+        return True
+    board[y][x]=2
+    for i in range(4):
+        ty=y+my[i]
+        tx=x+mx[i]
+        if 0<=ty<m and 0<=tx<n and not board[ty][tx]:
+            if per(ty,tx): return True
+    return False
 
-bl=True
 for i in range(n):
-    q.clear()
     if not board[0][i]:
-        q.append(0)
-        q.append(i)
-        if per():
-            print("YES")
-            bl=False
+        if per(0,i):
+            ans="YES"
             break
-            
-if bl: print("NO")
+
+print(ans)
