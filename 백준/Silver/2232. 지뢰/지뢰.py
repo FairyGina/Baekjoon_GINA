13da6dd606c4b7dc51b12c51b18886cb9b6e14bf
GINA <101798990+FairyGina@users.noreply.github.com>
2025-07-26 10:54:37 +0900

    [Silver II] Title: 지뢰, Time: 76 ms, Memory: 36484 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.py
+++ 백준/Silver/2232. 지뢰/지뢰.py
@@ -1,27 +1,10 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
-
 op=[]
 n=int(input())
-mine=[int(input()) for _ in range(n)]
-
-sv,id,mid=0,0,0
-bl=False
-
-while id<n:
-    if not bl:
-        if sv==mine[id]:
-            op.append(str(id))
-            id-=1
-            mine[id]=0
-        elif sv>mine[id]:
-            op.append(str(id))
-            bl=True
-    elif bl:
-        if sv<=mine[id]:
-            bl=False
-    sv=mine[id]
-    id+=1
-
-if not bl: op.append(str(n))
+m=[0]*(n+2)
+for i in range(1,n+1): m[i]=int(input())
+for i in range(1,n+1):
+    if m[i-1]<=m[i]>=m[i+1]:
+        op.append(str(i))
 print('\n'.join(op))
