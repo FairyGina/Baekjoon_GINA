62462080506b1fcbd4ee7ba9640e5af5ad973f17
GINA <101798990+FairyGina@users.noreply.github.com>
2025-07-29 05:29:14 +0900

    [Silver I] Title: 오목, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,51 +1,35 @@
 import sys
-from collections import deque
+my=(0,1,1,-1)
+mx=(1,0,1,1)
 
-def input(): return sys.stdin.readline().rstrip()
-def om(r):
-    if q.pop()==r: return False
-    for _ in range(5):
-        if q.pop()!=r: return False
-    if q.pop()==r: return False
-    return True
-
-def qwer(r):
-    global ar,ac
+def om(nb):
     for row in range(21):
-        for col in range(15):
-            for i in range(7):
-                q.append(board[row][col+i])
-            if om(r): return row,col+1
-            
-    for row in range(15):
         for col in range(21):
-            for i in range(7):
-                q.append(board[row+i][col])
-            if om(r): return row+1,col
-            
-    for row in range(15):
-        for col in range(15):
-            for i in range(7):
-                q.append(board[row+i][col+i])
-            if om(r): return row+1,col+1
-            
-    for row in range(6,21):
-        for col in range(15):
-            for i in range(7):
-                q.append(board[row-i][col+i])
-            if om(r): return row-1,col+1
-    
+            for m in range(4):
+                ct=0
+                if board[row][col]==nb: continue
+                for i in range(1,6):
+                    r=row+i*my[m]
+                    c=col+i*mx[m]
+                    if 0<=r<21 and 0<=c<21 and board[r][c]==nb:
+                        ct+=1
+                    else: break
+                r=row+6*my[m]
+                c=col+6*mx[m]
+                if ct==5:
+                    if 0<=r<21 and 0<=c<21:
+                        if board[r][c]!=nb: return row+my[m],col+mx[m]
+                    else: return row+my[m],col+mx[m]
     return -1,-1
-
-q=deque()
+                    
 board=[[-1]+list(map(int,input().split(" ")))+[-1] for _ in range(19)]
 board=[[-1]*21]+board+[[-1]*21]
 
-a,b=qwer(1)
+a,b=om(1)
 if a!=-1:
     print(f"1\n{a} {b}")
 else:
-    a,b=qwer(2)
+    a,b=om(2)
     if a!=-1:
         print(f"2\n{a} {b}")
     else: print(0)
