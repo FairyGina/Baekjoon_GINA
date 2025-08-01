1348b838382003102f0384ea40dfa79981c5f8bf
GINA <101798990+FairyGina@users.noreply.github.com>
2025-08-01 14:20:39 +0900

    [Silver II] Title: 이동하기, Time: 476 ms, Memory: 41096 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,17 +1,13 @@
 import sys
+from collections import deque
 def input(): return sys.stdin.readline().rstrip()
-my=(0,-1,-1)
-mx=(-1,-1,0)
+
 n,m=map(int,input().split())
 miro=[list(map(int,input().split(" "))) for _ in range(n)]
+dp=[0]*(m+1)
+
 for r in range(n):
     for c in range(m):
-        mct=miro[r][c]
-        for v in range(3):
-            ty=r+my[v]
-            tx=c+mx[v]
-            if 0<=ty<n and 0<=tx<m:
-                tm=miro[ty][tx]+miro[r][c]
-                if tm>mct: mct=tm
-        miro[r][c]=mct
-print(miro[n-1][m-1])
+        dp[c+1]=max(dp[c],dp[c+1])+miro[r][c]
+        
+print(dp[m])
