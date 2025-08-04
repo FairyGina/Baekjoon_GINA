d4209f833025058602bcac086db5f4509335e356
GINA <101798990+FairyGina@users.noreply.github.com>
2025-08-04 16:03:49 +0900

    [Silver I] Title: 트럭, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -1,26 +1,20 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
+
 n,w,l=map(int,input().split(" "))
 truck=list(map(int,input().split(" ")))
-wei=[0]*1000
-ct=[0]*1000
-id,time,front,back,num,lt=0,1,0,0,0,0
-bl=True
-while bl:
+ct=[0]*n
+
+time,front,back,num,lt=0,0,0,0,0
+while front<n:
     time+=1
-    if lt<w and id<n and (num+truck[id])<=l:
+    if ct[front]==time:
+        num-=truck[front]
+        front+=1
+    if back<n and (num+truck[back])<=l:
+        num+=truck[back]
+        ct[back]=time+w
         back+=1
-        wei[back-1]=truck[id]
-        num+=truck[id]
-        lt+=1
-        id+=1
-    for i in range(front,back):
-        ct[i]+=1
-    for i in range(front,back):
-        if ct[i]==w:
-            num-=truck[front]
-            front+=1
-            lt-=1
-            if id==n and lt<1: bl=False
-        else: break
+    elif front<n:
+        time=ct[front]-1
 print(time)
