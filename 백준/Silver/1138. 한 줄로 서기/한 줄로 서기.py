150ea30d613151c745204d2aa9d21cb77b0f345c
GINA <101798990+FairyGina@users.noreply.github.com>
2025-08-10 11:26:33 +0900

    [Silver II] Title: 한 줄로 서기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -1,15 +1,12 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
-
 n=int(input())
 hei=list(map(int,input().split()))
 nb=[0]*n
-
 for i in range(n):
-    ht=hei[i]
     ct=0
     for j in range(n):
-        if ct==ht:
+        if ct==hei[i]:
             for k in range(j,n):
                 if nb[k]==0:
                     nb[k]=i+1
