48a5331b8338d9a055c3acadd0b1ecbc51e47716
GINA <101798990+FairyGina@users.noreply.github.com>
2025-08-27 03:21:52 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 204 ms, Memory: 49412 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -1,29 +1,14 @@
 import sys
+import bisect
 def input(): return sys.stdin.readline().rstrip()
 op=[]
-def find(ip):
-    global l
-    lt,rt=0,l-1
-    while lt<=rt:
-        m=(lt+rt)//2
-        if ip==arr[m][0]: return arr[m][1]
-        elif ip<arr[m][0]:
-            rt=m-1
-        else: lt=m+1
-    return arr[lt][1]
 n,m=map(int,input().split())
-arr=[]
-ho=set()
-for i in range(n):
+arr,lab=[],[]
+for _ in range(n):
     name,at=input().split()
-    if at not in ho:
-        ho.add(at)
-        ta=[]
-        ta.append(int(at))
-        ta.append(name)
-        arr.append(ta)
-l=len(ho)
-arr.sort(key=lambda x:x[0])
+    lab.append(name)
+    arr.append(int(at))
 for _ in range(m):
-    op.append(find(int(input())))
+    idx=bisect.bisect_left(arr,int(input()))
+    op.append(lab[idx])
 print('\n'.join(op))
