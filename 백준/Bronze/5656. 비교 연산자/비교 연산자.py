29f969835914cfc492fc25ddd48cd2e4df3391b9
GINA <101798990+FairyGina@users.noreply.github.com>
2025-08-29 01:18:03 +0900

    [Bronze II] Title: 비교 연산자, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/5656. 비교 연산자/비교 연산자.py
+++ 백준/Bronze/5656. 비교 연산자/비교 연산자.py
@@ -5,25 +5,23 @@ t=0
 while True:
     t+=1
     ip=input().split()
+    a,b=int(ip[0]),int(ip[2])
     if ip[1]=="E": break
     mk=f"Case {t}: "
-    if ip[1]=='>': 
-        if int(ip[0])>int(ip[2]): mk+="true"
-        else: mk+="false"
-    elif ip[1]=='>=': 
-        if int(ip[0])>=int(ip[2]): mk+="true"
-        else: mk+="false"
-    elif ip[1]=='<': 
-        if int(ip[0])<int(ip[2]): mk+="true"
-        else: mk+="false"
-    elif ip[1]=='<=': 
-        if int(ip[0])<=int(ip[2]): mk+="true"
-        else: mk+="false"
-    elif ip[1]=='==': 
-        if int(ip[0])==int(ip[2]): mk+="true"
-        else: mk+="false"
-    else: 
-        if int(ip[0])!=int(ip[2]): mk+="true"
-        else: mk+="false"
+    if ip[1]=='>':
+        bl=a>b
+    elif ip[1]=='>=':
+        bl=a>=b
+    elif ip[1]=='<':
+        bl=a<b
+    elif ip[1]=='<=':
+        bl=a<=b
+    elif ip[1]=='==':
+        bl=a==b
+    else:
+        bl=a!=b
+    if bl==True:
+        mk+="true"
+    else: mk+="false"
     op.append(mk)
 print('\n'.join(op))
