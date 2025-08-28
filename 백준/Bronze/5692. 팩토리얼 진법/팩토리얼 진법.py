59667b979eb073fa5914b31847688ac31a4916be
GINA <101798990+FairyGina@users.noreply.github.com>
2025-08-29 01:49:30 +0900

    [Bronze III] Title: 팩토리얼 진법, Time: 148 ms, Memory: 38724 KB -BaekjoonHub

--- 백준/Bronze/5692. 팩토리얼 진법/팩토리얼 진법.py
+++ 백준/Bronze/5692. 팩토리얼 진법/팩토리얼 진법.py
@@ -2,12 +2,12 @@ import sys
 def input(): return sys.stdin.readline().rstrip()
 op=[]
 while True:
-    ip=input()
-    if ip=='0': break
-    fac,pl,ct=1,1,0
-    for i in range(len(ip)-1,-1,-1):
-        fac*=pl
-        pl+=1
-        ct+=fac*int(ip[i])
-    op.append(str(ct))
+    ip=int(input())
+    if ip==0: break
+    q=ip//10000
+    w=ip//1000%10
+    e=ip//100%10
+    r=ip//10%10
+    g=ip%10
+    op.append(str(120*q+24*w+6*e+2*r+g))
 print('\n'.join(op))
