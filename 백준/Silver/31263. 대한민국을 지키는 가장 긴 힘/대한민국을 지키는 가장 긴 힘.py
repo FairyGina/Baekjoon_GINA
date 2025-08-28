0cd1632fd70dddbc813e446f704f39d96f3af6f7
GINA <101798990+FairyGina@users.noreply.github.com>
2025-08-28 16:48:00 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 48 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
@@ -1,14 +1,15 @@
 import sys
+def input(): return sys.stdin.readline().rstrip()
 lt=int(input())
 ip=input()
-str,idx,ct="",lt-1,0
-while idx>-1:
-    tn=ip[idx]
-    if ip[idx]=='0': tn="10"
-    if int(tn+str)>641:
-        ct+=1
-        str=""
-    str=ip[idx]+str
-    idx-=1
-if str!="": ct+=1
-print(ct)
+if lt==1 or lt==2: print(1)
+else:
+    dp=[0]*(lt+1)
+    dp[1]=dp[2]=1
+    for i in range(3,lt+1):
+        dp[i]=dp[i-1]+1
+        two=int(ip[i-2:i])
+        thr=int(ip[i-3:i])
+        if two>9: dp[i]=min(dp[i],dp[i-2]+1)
+        if 99<thr<642: dp[i]=min(dp[i],dp[i-3]+1)
+    print(dp[lt])
