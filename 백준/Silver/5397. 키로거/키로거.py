c1fd2972bd0efc7c19f6af50f971dc4b6e634c12
GINA <101798990+FairyGina@users.noreply.github.com>
2025-09-05 23:58:29 +0900

    [Silver II] Title: 키로거, Time: 832 ms, Memory: 47816 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -5,12 +5,12 @@ for _ in range(int(input())):
     s=input()
     left,right=[],[]
     for c in s:
-        if c=='<' and left:
-            right.append(left.pop())
-        elif c=='>' and right:
-            left.append(right.pop())
-        elif c=='-'and left:
-            left.pop()
-        elif 'a'<=c<='z' or 'A'<=c<='Z' or '0'<=c<='9': left.append(c)
+        if c=='<':
+            if left: right.append(left.pop())
+        elif c=='>':
+            if right: left.append(right.pop())
+        elif c=='-':
+            if left: left.pop()
+        else: left.append(c)
     op.append(''.join(left)+''.join(right[::-1]))
 print('\n'.join(op))
