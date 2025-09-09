bbce13f6d563d90153232032c80a9e90dfb82d02
GINA <101798990+FairyGina@users.noreply.github.com>
2025-09-09 23:38:36 +0900

    [Silver II] Title: Everlasting...?, Time: 1016 ms, Memory: 43296 KB -BaekjoonHub

--- 백준/Silver/13817. Everlasting．．．？/Everlasting．．．？.py
+++ 백준/Silver/13817. Everlasting．．．？/Everlasting．．．？.py
@@ -1,26 +1,26 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
+def fd(n):
+    num=0
+    st=set()
+    while True:
+        if n==pf[n]:
+            num+=n
+            if n in st: num+=n
+            return num
+        if not pf[n] in st: num-=pf[n]
+        st.add(pf[n])
+        n//=pf[n]
 nlm=10**6+1
-pf,fd=[0]*nlm,[0]*nlm
+pf=[0]*nlm
 for i in range(2,nlm):
     if pf[i]==0: pf[i]=i
     for j in range(i*i,nlm,i):
         if pf[j]<1: pf[j]=i
-for i in range(2,nlm):
-    n,num=i,0
-    st=set()
-    while True:
-        if n==pf[n]:
-            fd[i]=num+n
-            if n in st: fd[i]+=n
-            break
-        if not pf[n] in st: num-=pf[n]
-        st.add(pf[n])
-        n//=pf[n]
 op=[]
 while True:
     a,b=map(int,input().split())
     if a==0: break
-    if fd[a]>fd[b]: op.append('a')
+    if fd(a)>fd(b): op.append('a')
     else: op.append('b')
 print('\n'.join(op))
