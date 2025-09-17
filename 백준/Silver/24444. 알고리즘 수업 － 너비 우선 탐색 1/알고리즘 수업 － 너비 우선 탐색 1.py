e9d240df589b5b59b1085b8fa660a872226ec485
GINA <101798990+FairyGina@users.noreply.github.com>
2025-09-18 03:50:32 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 532 ms, Memory: 68012 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+def input(): return sys.stdin.readline().rstrip()
+n,m,r=map(int,input().split())
+nb=list([] for _ in range(n+1))
+vt=[0]*(n+1)
+for i in range(m):
+    a,b=map(int,input().split())
+    nb[a].append(b)
+    nb[b].append(a)
+for i in range(n): nb[i].sort()
+dq=deque()
+dq.append([r])
+ct=0
+while dq:
+    for i in dq.popleft():
+        if vt[i]<1:
+            dq.append(nb[i])
+            ct+=1
+            vt[i]=ct
+print('\n'.join(map(str,vt[1:])))
