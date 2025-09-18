094e7fa2c46380e9f01e42d8e32afde396606a49
GINA <101798990+FairyGina@users.noreply.github.com>
2025-09-19 06:07:32 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 120 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,32 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+def bfs():
+    board=list([0]*10 for _ in range(10))
+    n,m=map(int,input().split())
+    for _ in range(n):
+        a,b=map(lambda x:int(x)-1,input().split())
+        board[a//10][a%10]=b
+    for _ in range(m):
+        a,b=map(lambda x:int(x)-1,input().split())
+        board[a//10][a%10]=b
+    board[0][0]=-1
+    dq,tq=[],[]
+    dq.append(0)
+    me,ct=0,0
+    while dq:
+        tq.clear()
+        while dq:
+            me=dq.pop()
+            lm,mb=min(99,me+6),me
+            if me==99: return ct
+            for i in range(lm,lm-6,-1):
+                y,x=i//10,i%10
+                if board[y][x]<0: continue
+                if board[y][x]<1: mb=max(mb,i)
+                else:
+                    ty,tx=board[y][x]//10,board[y][x]%10
+                    if board[ty][tx]==0: tq.append(board[y][x])
+            if me<mb: tq.append(mb)
+        dq=list(tq)
+        ct+=1
+print(bfs())
