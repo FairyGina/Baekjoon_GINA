12d77fa6bd0afd728f069a771b0259f80a33bbc5
GINA <101798990+FairyGina@users.noreply.github.com>
2025-09-26 11:43:42 +0900

    [Silver II] Title: 나무 자르기, Time: 1104 ms, Memory: 145580 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,34 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+def bs(tr):
+    lf,rt=0,n
+    while lf<rt:
+        mid=(lf+rt)//2
+        if tree[mid]<tr: rt=mid
+        elif tree[mid]>tr: lf=mid+1
+        else: return mid
+    return lf
+    
+def fd():
+    lf,rt=0,tree[0]
+    ans=0
+    while lf<=rt:
+        mid=(lf+rt)//2
+        idx=bs(mid)
+        res=num[idx]-idx*mid
+        if res>=m:
+            lf=mid+1
+            ans=mid
+        else: rt=mid-1
+    return ans
+
+n,m=map(int,input().split())
+tree=list(map(int,input().split()))
+tree.append(0)
+tree.sort(reverse=True)
+
+num=[0]*(n+1)
+for i in range(n):
+    num[i+1]+=tree[i]+num[i]
+print(fd())
