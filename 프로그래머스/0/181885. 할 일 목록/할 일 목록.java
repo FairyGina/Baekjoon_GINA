ee685f54d33f798b2c559a3c68d7ab7c63b54054
GINA <101798990+FairyGina@users.noreply.github.com>
2025-11-09 01:52:24 +0900

    [level 0] Title: 할 일 목록, Time: 1.85 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> arr=new ArrayList<>();
+        for(int i=0;i<finished.length;i++){
+            if(!finished[i]) arr.add(todo_list[i]);
+        }
+        
+        String[] answer = arr.stream().toArray(String[]::new);
+        return answer;
+    }
+}
