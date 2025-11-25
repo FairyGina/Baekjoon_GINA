687e6bfbcb82d1bab8070f11f5bf555ea111c428
GINA <101798990+FairyGina@users.noreply.github.com>
2025-11-25 18:28:42 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,17 +1,10 @@
 import java.io.*;
-import java.util.*;
 class Main {
-    static String s;
-    static int ct=0;
-    public static void f1(String str1){
-        s=s.replace(str1," ");
-        int ln=s.length();
-    }
     public static void main(String[] args) throws Exception{
         BufferedReader I=new BufferedReader(new InputStreamReader(System.in));
-        s=I.readLine();
+        String s=I.readLine();
         String[] arr={"c=","c-","dz=","d-","lj","nj","s=","z="};
-        for(String a:arr) f1(a);
-        System.out.print(ct+s.length());
+        for(String a:arr) s=s.replace(a," ");
+        System.out.print(s.length());
     }
 }
