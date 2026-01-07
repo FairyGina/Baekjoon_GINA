cb6af49b99d9533b0655b1809fec1e3a4d9d754e
GINA <101798990+FairyGina@users.noreply.github.com>
2026-01-07 22:32:50 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 276 ms, Memory: 32412 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -1,8 +1,10 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
+
 lm=1000000007
 n=int(input())
-a,b,c=0,1,0
+a,b,c=0,1,1
+if n==0: c=0
 for i in range(2,n+1):
     c=(a+b)%lm
     a=b
