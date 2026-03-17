e47308bfa9aac3d6af682d59807682e9059857e3
GINA <101798990+FairyGina@users.noreply.github.com>
2026-03-17 11:47:09 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,16 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+dp=[0]*101
+dp[1]=1
+dp[2]=1
+dp[3]=1
+
+for i in range(4,101):
+    dp[i]=dp[i-2]+dp[i-3]
+
+op=[]
+for _ in range(int(input())):
+    op.append(str(dp[int(input())]))
+
+print('\n'.join(op))
