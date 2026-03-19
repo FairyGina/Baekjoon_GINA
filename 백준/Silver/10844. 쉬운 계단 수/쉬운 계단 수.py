d74a0511cc01d0d3935b8afb6221005d7cf1ea6f
GINA <101798990+FairyGina@users.noreply.github.com>
2026-03-19 11:12:49 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -4,16 +4,19 @@ def input(): return sys.stdin.readline().rstrip()
 n=int(input())
 lm=1000000000
 
-sv=[0]*12
-dp=[0]*12
+sv1=[0]*12
+
+dp1=[0]*12
 
 for i in range(2,11):
-    sv[i]=1
+    sv1[i]=1
 
 for _ in range(1,n):
     for j in range(1,11):
-        dp[j]=sv[j-1]+sv[j+1]
-    sv=dp.copy()
-    dp=[0]*12
+        dp1[j-1]+=sv1[j]
+        dp1[j+1]+=sv1[j]
+    sv1=dp1.copy()
+    dp1=[0]*12
 
-print(sum(sv[1:11])%lm)
+
+print(sum(sv1[1:11])%lm)
