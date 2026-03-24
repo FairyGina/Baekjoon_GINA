0fce1103c383976e5f16891cb4b155441a065b55
GINA <101798990+FairyGina@users.noreply.github.com>
2026-03-24 17:24:17 +0900

    [Silver V] Title: Crazy_aRcade_Good, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17290. Crazy＿aRcade＿Good/Crazy＿aRcade＿Good.py
+++ 백준/Silver/17290. Crazy＿aRcade＿Good/Crazy＿aRcade＿Good.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 import sys
 def input(): return sys.stdin.readline().rstrip()
 
@@ -7,18 +5,21 @@ py,px=map(int,input().split())
 py,px=py-1,px-1
 
 board=[list(input()) for _ in range(10)]
+vt=[[0]*10 for _ in range(2)]
 
-dq=deque()
 for i in range(10):
     for j in range(10):
         if board[i][j]=='o':
-            dq.append((i,j))
+            vt[0][i]=1
+            vt[1][j]=1
 
-while dq:
-    y,x=dq.pop()
-    for i in range(10):
-        board[y][i]='o'
-        board[i][x]='o'
+for i in range(10):
+    if vt[0][i]:
+        for j in range(10):
+            board[i][j]='o'
+    if vt[1][i]:
+        for j in range(10):
+            board[j][i]='o'
 
 minct=10**9
 for i in range(10):
